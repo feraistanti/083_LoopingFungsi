ddd3ede91c633a733f1d7c47ef15d3ffd34c4785
feraistanti <fistanti551@gmail.com>
2024-05-27 16:11:31 +0700

    memasukkan remot lampu ruangan

--- ParadigmaOOP2_0083/EncapSulation.cpp
+++ ParadigmaOOP2_0083/EncapSulation.cpp
@@ -13,3 +13,18 @@ public:
     }
 };
 
+int main() {
+    remoteLampu LampuRumah;
+
+    LampuRumah.setSakLarNo(0, "Lampu Teras Rumah");
+    LampuRumah.setSakLarNo(1, "Lampu Ruang Tamu");
+    LampuRumah.setSakLarNo(2, "Lampu Kamar Tidur");
+    LampuRumah.setSakLarNo(3, "Lampu Dapur");
+
+    cout << LampuRumah.getSakLarNo(0) << endl;
+    cout << LampuRumah.getSakLarNo(1) << endl;
+    cout << LampuRumah.getSakLarNo(2) << endl;
+    cout << LampuRumah.getSakLarNo(3) << endl;
+
+    return 0;
+}
